9bbbb8d9d018e492434865c98c3178024043c7e9
Admin <petrodavidenko32@gmail.com>
2022-03-20 01:25:07 +0200

    Initial commit

---
+++ src/test_while_2.java
@@ -0,0 +1,9 @@
+public class test_while_2 {
+    public static void main(String[] args) {
+        int a = 100;
+        while (a >= 10) {
+            System.out.println("Обратный отсчет: " + a);
+            a-=10;
+        }
+    }
+}
